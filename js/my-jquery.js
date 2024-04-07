a7c188870c7ee82400af3512877b8ef8dc8b4b59
AIRI830 <166088766+AIRI830@users.noreply.github.com>
2024-04-07 12:49:38 +0900

    20240407

---
+++ js/my-jquery.js
@@ -0,0 +1,72 @@
+// JavaScript source code
+
+/*画像サムネイルクリック時にビューワー表示*/
+//$(function () {
+//   /* $(".gallery_block div").css("background", "#cccccc");*/
+//    $(".gallery_block div").click(function () {
+//        $('.optionBox').css("display", "block");
+//    }); 
+
+//});
+
+/*ビューワークリック時にビューワー非表示*/
+//$(function () {
+//    $('.optionBox').click(function () {
+//        $('.optionBox').css("display", "none");
+//    }); 
+//});
+
+
+/*$(document).click(function(event) {*/
+  // event.targetをjQueryオブジェクトに変換する
+  // closest()を使って自分から先祖要素までinnerクラスがある要素を選択する
+//    if (!$(event.target).closest('.optionBox').length) {
+//    console.log('外側がクリックされました。');
+//  }
+//});
+
+// 要素が画面内に入ったらclass付与
+$(window).scroll(function () {
+    $('.about').each(function () {
+        var hit = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var wHeight = $(window).height();
+        var customTop = 10;
+        if (typeof $(this).data('about') !== 'undefined') {
+            // data-effect="任意の値" が設定されている場合
+            customTop = $(this).data('about');
+        }
+        if (hit + customTop < wHeight + scroll) {
+            $(this).addClass("catch_copy");
+        }
+    });
+});
+
+/*ページTOPに戻るボタン*/
+$(function () {
+    var pagetop = $('#page-top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
+
+/*メール暗号化*/
+$(document).ready(function () {
+    var mailadd = "akkikkakaakkaak.kaaaakakaakkkaamkkakaaakaakak";
+    var mailadd_2 = mailadd.replace(/k/g, "");
+  /*  $(".mail-contact").text(mailadd_2);*/
+    $(".mail-contact").append('<div class="aa"></div>');
+    $(".mail-contact").attr("href", "mailto:" + mailadd_2);
+});
+
+
+
